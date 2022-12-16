393facc66d8725f30794716f5306333f6be44ec0
João Aiub <joaoribeiroaiub@gmail.com>
2022-12-16 02:40:54 -0300

    aulas dia 16/12/2022

---
+++ JS_MODERNO/34-usando-o-webpack/webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  entry: {
+    main: './src/index.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+  }]
+}
+}
