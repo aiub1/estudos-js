16007b5641b305db998dd0fa9c978cd9bcf1d717
João Aiub <joaoribeiroaiub@gmail.com>
2022-12-24 17:57:18 -0300

    aulas dis 22/12/2022

---
+++ JS_POO/11-encapsulamento/Account.js
@@ -0,0 +1,30 @@
+class Account {
+  #password
+  #balance = 0
+  constructor(user) {
+    this.email = user.email
+    this.#password = user.password
+  }
+
+  getBalance(email, password) {
+    if (this.#authenticate(email, password)) {
+      return this.#balance
+    }else{
+      return null
+    }
+  }
+
+  #authenticate(email, password) {
+    return this.email === email && this.#password === password
+  }
+}
+
+const User = {
+  email: "isaac@email.com",
+  password: "123456"
+}
+
+const myAccount = new Account (User)
+
+console.log(myAccount)
+console.log(myAccount.getBalance("isaac@email.com","123456"))
