1db1e28dbef2340e41b08ad96bc1d942c295326f
João Aiub <joaoribeiroaiub@gmail.com>
2023-01-06 01:37:06 -0300

    estudos 06/01/2023

---
+++ JS_POO/23-exercicio-22/entities/Installment.js
@@ -0,0 +1,5 @@
+module.exports = class Installment {
+  constructor(value, number, situation){
+    
+  }
+}
--- JS_POO/23-exercicio-22/entities/Loan.js
+++ JS_POO/23-exercicio-22/entities/Loan.js
@@ -1,12 +1,20 @@
+const Installment = require("./Installment")
+
 module.exports = class Loan {
-  #installmentsValue
-  constructor (value, installmentsNumber) {
+
+  constructor(value,installmentNumber) {
     this.value = value
-    this.installmentsNumber = installmentsNumber
     this.date = new Date()
+    this.instalement = new Installment()
   }
 
-  static interestFee = 0.08
+  static #interestFee = 0.08
 
+  static get interestFeeValue() {
+   return Loan.#interestFee
+  }
 
+  set interestFee(newInterestFee) {
+    this.#interestFee = newInterestFee/100
+  }
 }
