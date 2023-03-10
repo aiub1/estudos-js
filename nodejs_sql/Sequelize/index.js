a0be03803b6bd3e1c58ce01be18efc3fadc789f8
João Aiub <joaoribeiroaiub@gmail.com>
2023-03-10 01:00:13 -0300

    estudos 10/03/2023

--- nodejs_sql/Sequelize/index.js
+++ nodejs_sql/Sequelize/index.js
@@ -1,9 +1,25 @@
 (async () => {
   const Planet = require("./models/Planet")
-
+/*
   const newPlanet = await Planet.create({
     name: "Venus",
     position: 5,
   })
-  console.log(newPlanet);
+
+  const seePlanets = await Planet.findAll({
+    where: {
+      name: "Venus"
+    }
+  })
+  
+  const updatePlanets = await Planet.findByPk(3)
+  updatePlanets.position = 2
+  await updatePlanets.save()
+  */
+
+ const deletePlanets = await Planet.findByPk(4)
+ console.log(deletePlanets);
+
+ await deletePlanets.destroy()
+ console.log(deletePlanets);
 })();
